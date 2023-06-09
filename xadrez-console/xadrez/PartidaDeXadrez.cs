740de1478cd6db8520466202bf00a5e80a43b37e
Gleder <glederson_cf@yahoo.com.br>
2023-06-09 09:12:11 -0300

    Correcao: mudanca de nome de metodo

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -99,7 +99,7 @@ namespace xadrez
 
         public void ValidarPosicaoDeDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).PodeMoverPara(destino))
+            if (!tab.peca(origem).MovimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posicao de destino invalida!");
             }            
